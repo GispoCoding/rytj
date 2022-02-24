65867d7e8bc5504a847a873cbe79081a01fe6ab5
Santtu Pyykkönen <santtupyykkonen@pop-os.localdomain>
2022-02-24 12:11:26 +0200

    change geom-name, modify koontinakymat-queries

--- koontinakymat/rytj_koontinakymat.sql
+++ koontinakymat/rytj_koontinakymat.sql
@@ -1,68 +1,74 @@
 -- luo skeema
-CREATE SCHEMA koontinakymat;
+CREATE SCHEMA IF NOT EXISTS koontinakymat;
 
 -- luo kokoava taso kaavakohteista ja niihin liittyvistä lisätiedoista
--- DROP MATERIALIZED VIEW IF EXISTS koontinakymat.kaavakohteet_mv 
-CREATE MATERIALIZED VIEW koontinakymat.kaavakohteet_mv AS (
-SELECT 
-    ROW_NUMBER() OVER (ORDER BY k.id) AS id, 
+DROP MATERIALIZED VIEW IF EXISTS koontinakymat.kaavakohteet_mv_latest; 
+CREATE MATERIALIZED VIEW koontinakymat.kaavakohteet_mv_latest AS ( 
+    SELECT DISTINCT ON (k2.id) k2.id,
+    ROW_NUMBER() OVER (ORDER BY k.id) AS fid,
     k.id AS id_kaavakohde,
-    kla.nimi_fi, 
+    kav.nimi_fi AS kaavoitusteema,
     kla.nimi_fi AS kaavamaarayslaji,
+    t2.arvo AS tekstiarvo,
+    na.arvo AS numeerinen_arvo,
+    na.mittayksikko AS mittayksikko,
+    t.nimi AS tietoyksikko_nimi,
     s.nimi_fi AS sitovuuslaji,
     ml.nimi_fi AS maanalaisuuden_laji,
-    t.nimi AS tietoyksikko_nimi,
-    t2.arvo AS tekstiarvo,
-    na.arvo AS tietoyksikko_num_arvo,
-    na.mittayksikko AS tietoyksikko_num_mittayksikko,
-    st_collect(st_curvetoline(geom), geom_polygon) AS geom 
+    st_curvetoline(geom) AS geom
     FROM kaavatiedot.kaavakohde k
-        LEFT JOIN kaavatiedot.tietoyksikko t ON k.id = t.id_kaavakohde
+        LEFT JOIN kaavatiedot.kaavamaarays k2 ON k.id = k2.id_kaavakohde  
+        LEFT JOIN kaavatiedot.tietoyksikko t ON k2.id = t.id_kaavamaarays
         LEFT JOIN kaavatiedot.numeerinen_arvo na ON t.id = na.id_tietoyksikko 
-        LEFT JOIN kaavatiedot.kaavamaarays k2 ON k.id = k2.id_kaavakohde 
+        LEFT JOIN kaavatiedot.tekstiarvo t2 ON t.id = t2.id_tietoyksikko       
+        LEFT JOIN koodistot.kaavoitusteema_ak kav ON k2.id_kaavoitusteema_ak = kav.jarjestys 
         LEFT JOIN koodistot.kaavamaarays_laji_ak kla ON k2.id_kaavamaarays_laji_ak = kla.jarjestys
         LEFT JOIN koodistot.sitovuuslaji s ON k.id_sitovuuslaji = s.jarjestys
         LEFT JOIN koodistot.maanalaisuuden_laji ml ON k.id_maanalaisuuden_laji = ml.jarjestys
-        LEFT JOIN kaavatiedot.tekstiarvo t2 ON t.id = t2.id_tietoyksikko 
-    );
-
+        ORDER BY k2.id
+        );
+    
 -- indeksi
-CREATE INDEX koontinakymat_kaavakohteet_mv_geom_idx ON koontinakymat.kaavakohteet_mv USING gist (geom);      
+CREATE INDEX IF NOT EXISTS koontinakymat_kaavakohteet_mv_geom_idx ON koontinakymat.kaavakohteet_mv_latest USING gist (geom);      
 
+ 
 -- hae kaikki kaavamääräykset, niihin liittyvän kaavakohteen geometriat ja ryhmitä ne (group by)
-CREATE MATERIALIZED VIEW koontinakymat.kaavamaaraykset_mv AS (
+DROP MATERIALIZED VIEW IF EXISTS koontinakymat.kaavamaaraykset_mv_latest; 
+CREATE MATERIALIZED VIEW koontinakymat.kaavamaaraykset_mv_latest AS (
     SELECT 
         ROW_NUMBER() OVER (ORDER BY kla.id) AS id,
         kla.nimi_fi AS kaavamaarayslaji,
-        st_union(st_makevalid(st_curvetoline (geom))) AS geom
-        /*,
-        st_union(geom_polygon) AS geom_polygon, --hae muutkin geom-sarakkeet tarvittaessa
-        st_union(geom_line) AS geom_line,
-        st_union(geom_point) AS geom_point
-        */
+        st_union(st_makevalid(st_curvetoline (k2.geom))) AS geom
         FROM kaavatiedot.kaavamaarays k 
             LEFT JOIN koodistot.kaavamaarays_laji_ak kla ON k.id_kaavamaarays_laji_ak = kla.jarjestys
-            LEFT JOIN kaavatiedot.kaavakohde k2 ON k.id_kaavakohde = k2.id
+            LEFT JOIN 
+                (SELECT kav.id,
+                st_curvetoline (kav.geom) AS geom
+                FROM kaavatiedot.kaavakohde kav) 
+                k2 ON k.id_kaavakohde = k2.id
         GROUP BY kla.nimi_fi, kla.id
-        ORDER BY geom);
+    );
 
 -- luo indeksi
-CREATE INDEX koontinakymat_kaavamaaraykset_mv_geom_idx ON koontinakymat.kaavamaaraykset_mv USING gist (geom);
+CREATE INDEX koontinakymat_kaavamaaraykset_mv_geom_idx ON koontinakymat.kaavamaaraykset_mv_latest USING gist (geom);
 
 -- luo kaavakohteet ja kaavamaaraykset -koontinäkymät
-CREATE MATERIALIZED VIEW koontinakymat.kaavakohteet_ja_kaavamaaraykset_mv AS (
+DROP MATERIALIZED VIEW IF EXISTS koontinakymat.kaavamaaraykset_listaus_mv_latest; 
+CREATE MATERIALIZED VIEW koontinakymat.kaavamaaraykset_listaus_mv_latest AS (
     SELECT 
         ROW_NUMBER() OVER (ORDER BY k1.id) AS id,     
         count(DISTINCT kla.nimi_fi) AS eri_kaavamaarayksien_maara,
         array_agg(kla.nimi_fi) AS kaavamaarayslajit,
-        st_collect(st_curvetoline(k1.geom), k1.geom_polygon) AS geom
+        st_curvetoline(k1.geom) AS geom
         FROM kaavatiedot.kaavakohde k1
             INNER JOIN kaavatiedot.kaavamaarays k ON k1.id = k.id_kaavakohde
             INNER JOIN koodistot.kaavamaarays_laji_ak kla ON k.id_kaavamaarays_laji_ak = kla.jarjestys
         WHERE geom IS NOT NULL
         GROUP BY k1.id
-)
+);
 
 -- luo indeksi
-CREATE INDEX koontinakymat_kaavakohteet_ja_kaavamaaraykset_mv_geom_idx ON koontinakymat.kaavakohteet_ja_kaavamaaraykset_mv USING gist(geom)
+CREATE INDEX IF NOT EXISTS koontinakymat_kaavamaaraykset_listaus_mv_geom_idx ON koontinakymat.kaavamaaraykset_listaus_mv_latest USING gist(geom)
+
+
 
